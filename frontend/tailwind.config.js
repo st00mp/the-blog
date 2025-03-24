d9a62c7cb024e41ebb2ad951c67195255d55770b
st00mp <st00mp@outlook.com>
2025-03-24 22:00:35 +0100

    update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('../colors')
 module.exports = {
     purge: [],
     presets: [],
-    darkMode: 'class', // or 'media' or 'class'
+    darkMode: 'class', // false or 'media' or 'class'
     theme: {
         screens: {
             sm: '640px',
