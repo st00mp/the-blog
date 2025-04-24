d7ab71a2c5b9da290bf72fe09b7a5c814b62f4fb
st00mp <st00mp@outlook.com>
2025-04-24 13:06:28 +0200

    update docker-compose pour php-my-admin multi-architecture et affichage d'un message d'erreur si .env et ses variables sont manquantes pour frontend/

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,17 @@
 import { NextConfig } from "next";
 
+// Vérification de la présence de BACKEND_API_URL
+if (!process.env.BACKEND_API_URL) {
+  throw new Error(
+    "❌ La variable BACKEND_API_URL est manquante.\n→ Crée un fichier `.env.local` à la racine de ton frontend avec :\nBACKEND_API_URL=http://nginx:80"
+  );
+}
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
     BACKEND_API_URL: process.env.BACKEND_API_URL,
+    NEXT_PUBLIC_BACKEND_API_URL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
   },
   async rewrites() {
     return [
@@ -16,3 +24,23 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+// import { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   env: {
+//     BACKEND_API_URL: process.env.BACKEND_API_URL,
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: `${process.env.BACKEND_API_URL}/api/:path*`,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
